0f7f4223a74ed5224901e679206d1565d90388f2
vishwas0612 <IIT2024087@iiita.ac.in>
2025-01-01 16:06:11 +0530

    Fix

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,9 +6,8 @@ const Display = () => {
   return (
     <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
-        <Route path='/' elements={<DisplayHome/>}/>
+        <Route path='/' element={<DisplayHome/>}></Route>
       </Routes>
-      <DisplayHome></DisplayHome>
     </div>
   )
 }
